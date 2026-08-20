d15731a6f573f5452bc1c8c91d4b2acae6317f5b
MEHEDI HASAN <mehedialhasan1999@gmail.com>
2026-08-20 14:21:50 +0600

    Add solution for LeetCode problem: Missing Number with multiple approaches

---
+++ leetcode/c++/binary search/268_Missing Number.cpp
@@ -0,0 +1,68 @@
+// problem link: https://leetcode.com/problems/missing-number/description/?envType=problem-list-v2&envId=binary-search
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// mathmatical approach
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int n = nums.size();
+        int sum = (n * (n + 1)) / 2;
+
+        for (int i = 0; i < n; i++)
+            sum -= nums[i];
+
+        return sum;
+    }
+};
+
+// XOR approach
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int n = nums.size();
+        int result = n;
+
+        for (int i = 0; i < n; i++)
+        {
+            result ^= i;
+            result ^= nums[i];
+        }
+
+        return result;
+    }
+};
+
+// binary search approach
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        int result = n;
+        int l = 0, r = n - 1;
+
+        while (l <= r)
+        {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] > mid)
+            {
+                result = mid;
+                r = mid - 1;
+            }
+            else
+                l = mid + 1;
+        }
+
+        return result;
+    }
+};
